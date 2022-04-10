3151f9425eeb8906e97db852ed7f6c1a6a7d8e7e
Larissa Barbosa <larissabarbosa@id.uff.br>
2022-04-10 12:01:14 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+let sec=0
+let min=0
+let hr=0
+let interval
+
+function twoDigits(digit){
+    if(digit<10){
+        return('0' + digit)
+    }else{
+        return(digit)
+    }
+}
+
+function start(){
+    watch()
+    interval = setInterval(watch, 1000)
+}
+
+function pause(){
+    clearInterval(interval)
+}
+
+function stop(){
+    clearInterval(interval)
+    sec=0
+    min=0
+    document.getElementById('watch').innerText=`00:00:00`
+}
+
+
+
+function watch(){
+    sec++
+    if(sec==60){
+        min++
+        sec=0
+        if(min==60){
+            min=0
+            hr++
+        }
+    }
+    document.getElementById('watch').innerText=`${twoDigits(hr)}:${twoDigits(min)}:${twoDigits(sec)}`
+}
